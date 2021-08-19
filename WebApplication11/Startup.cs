e75a4b99a9d80fed36139ecacd8a515f6406ef37
Srinivasa Rao Gummadilli <srinivas.gummadilli@gmail.com>
2021-08-19 18:06:08 +0530

    testing

--- WebApplication11/Startup.cs
+++ WebApplication11/Startup.cs
@@ -21,6 +21,7 @@ namespace WebApplication11
 
 
         //test
+        //test11
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
